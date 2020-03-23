4bc5875fcd110f754d91c03ccbc973cdaafebce3
Deblina <Deblina@DESKTOP-O1T1V2H>
2020-03-23 20:58:37 +0530

    First Commit

---
+++ Student-core-jdbc/src/main/java/com/cognizant/config/AppConfig.java
@@ -0,0 +1,55 @@
+package com.cognizant.config;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.cognizant.dao.StudentDao;
+
+import com.cognizant.dao.StudentDaoImpl;
+
+import com.cognizant.model.Student;
+
+
+@Configuration
+public class AppConfig {
+
+	@Bean("student")
+	public Student student()
+	{
+		return new Student();
+
+	}
+
+	@Bean("studao")
+	public StudentDao stdentDao()
+
+	{
+		return new StudentDaoImpl();
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource ds)
+
+	{
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(ds);
+
+		return jdbcTemplate;
+	}
+	@Bean
+	public DataSource dataSource()
+
+	{
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setUsername("root");
+		ds.setPassword("root12");
+		ds.setUrl("jdbc:mysql://localhost:3306/work");
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		return ds;
+	}
+}
---
+++ Student-core-jdbc/src/test/java/com/cognizant/AppTest.java
@@ -0,0 +1,61 @@
+package com.cognizant;
+
+
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.cognizant.config.AppConfig;
+import com.cognizant.dao.StudentDao;
+import com.cognizant.model.Student;
+
+
+
+public class AppTest {
+
+	private AnnotationConfigApplicationContext cntx;
+	private StudentDao st;
+	private Student s;
+
+
+	@Before
+	public void setup()
+
+	{
+		cntx = new AnnotationConfigApplicationContext(AppConfig.class);
+		st = (StudentDao) cntx.getBean("studao");
+		s = (Student) cntx.getBean("student");
+	}
+
+	
+
+	@Test
+	public void testInsert() {
+
+		s.setId(102);
+		s.setName("Hillol");
+		s.setMarks(20);
+
+		assertEquals("Success",st.insert(s));
+
+	}
+
+	
+
+	@Test
+	public void testGetAll() {
+
+		List<Student> stud = st.getAll();
+
+		assertEquals(2,stud.size());
+
+	}
+
+
+
+}
